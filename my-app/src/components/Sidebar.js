495739aa2b780e06ee2481c62821ee217e1037b2
oguzhanakca <oguzhan.akca@hotmail.com>
2024-08-11 18:21:22 +0200

    challenge one completed

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,9 @@
+function Sidebar() {
+    return <div>
+        <a href = "#.">My Photos</a>
+        <a href = "#.">My Illustrations</a>
+        <a href = "#.">My Paintings</a>
+    </div>
+}
+
+export default Sidebar

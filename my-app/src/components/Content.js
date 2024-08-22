992be19478022404c4514c0cad04f7926c609021
oguzhanakca <oguzhan.akca@hotmail.com>
2024-08-22 20:24:51 +0200

    content added

---
+++ my-app/src/components/Content.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import css from "./css/Content.module.css";
+import posts from "../posts.json";
+import PostItem from "./PostItem";
+
+export class Content extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className={css.Content}>
+        <div className={css.TitleBar}>
+          <h1>My Photos</h1>
+        </div>
+
+        <div className={css.SearchResults}>
+          <PostItem savedPosts={posts.savedPosts} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Content;

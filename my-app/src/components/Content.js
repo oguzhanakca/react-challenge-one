974a179eb9e468c842699136f067e07d6ebbdaf4
oguzhanakca <oguzhan.akca@hotmail.com>
2024-08-23 18:02:18 +0200

    LCM added

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,23 +2,43 @@ import React, { Component } from "react";
 import css from "./css/Content.module.css";
 import posts from "../posts.json";
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 export class Content extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      isLoaded: false,
+    };
+  }
+
+  loadPosts() {
+    setTimeout(() => {
+      this.setState({
+        isLoaded: true,
+      });
+    }, 2000);
+  }
+
+  componentDidMount() {
+    this.loadPosts();
   }
 
   render() {
     return (
+      // <Loader />
       <div className={css.Content}>
         <div className={css.TitleBar}>
           <h1>My Photos</h1>
         </div>
 
         <div className={css.SearchResults}>
-          <PostItem savedPosts={posts.savedPosts} />
+          {this.state.isLoaded ? (
+            <PostItem savedPosts={posts.savedPosts} />
+          ) : (
+            <Loader />
+          )}
         </div>
       </div>
     );
